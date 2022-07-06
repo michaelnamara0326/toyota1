6d7fb67204302f657aaf37aa2fd12c5352fcbaf3
Michael Shen <michaelshen@25sprout.com>
2022-07-06 23:19:02 +0800

    add icon and implement function button lay out (50%)

--- TOYOTA-HW/ViewController.swift
+++ TOYOTA-HW/ViewController.swift
@@ -17,36 +17,183 @@ class ViewController: UIViewController {
     let logoImageView: UIImageView = {
         let logoImageView = UIImageView()
         logoImageView.image = UIImage(named: "Toyota Logo White")
-        logoImageView.backgroundColor = .blue
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         return logoImageView
     }()
     let notifyButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "Action"), for: .normal)
-        button.backgroundColor = .blue
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     let bottomView: UIView = {
        let view = UIView()
-        view.backgroundColor = .yellow // switch to white
+        view.backgroundColor = .white // switch to white
+        view.layer.cornerRadius = 8.0
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    let fnButton: UIButton = {
+    let testButton:UIButton = {
         let btn = UIButton()
-        let btnBG = UIView()
-        return btn
+        let icon = UIImageView()
+        let titleLabel = UILabel()
+        let contentLabel = UILabel()
+        
+        btn.setImage(UIImage(named: "icon_img_background_left"), for: .normal)
+        icon.image = UIImage(named: "icon_img_filterCars")
+        titleLabel.font = UIFont(name: "PingFangTC-Semibold", size: 14)
+        contentLabel.font = UIFont(name: "PingFangTC-Regular", size: 12)
+        titleLabel.text = "速配車款"
+        contentLabel.text = "輸入條件篩選"
+        
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        btn.addSubview(icon)
+        btn.addSubview(titleLabel)
+        btn.addSubview(contentLabel)
+        
+        icon.topAnchor.constraint(equalTo: btn.topAnchor, constant: 16).isActive = true
+        icon.leadingAnchor.constraint(equalTo: btn.leadingAnchor, constant: 16).isActive = true
+        icon.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        icon.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: btn.topAnchor, constant: 11).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 12).isActive = true
+        titleLabel.widthAnchor.constraint(equalToConstant: 56).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        
+        contentLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+        contentLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 12).isActive = true
+        contentLabel.widthAnchor.constraint(equalToConstant: 72).isActive = true
+        contentLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        btn.backgroundColor = .blue // recognize
+        return btn
     }()
-    let stack: UIStackView = {
+    let testButton1:[UIButton] = {
+        var btn:[UIButton] = []
+    
+        let titleLabelArr:[String] = ["速配車款","立即預選"]
+        let contentLabelArr:[String] = ["輸入條件篩選", "預選下一台愛車"]
+        let iconArr:[String] = ["icon_img_filterCars", "icon_img_buycar"]
+        let bgArr:[String] = ["icon_img_background_left", "icon_img_background_right"]
+
+        for i in 0..<2{
+            let icon = UIImageView()
+            let titleLabel = UILabel()
+            let contentLabel = UILabel()
+            
+            icon.translatesAutoresizingMaskIntoConstraints = false
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            contentLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            titleLabel.font = UIFont(name: "PingFangTC-Semibold", size: 14)
+            contentLabel.font = UIFont(name: "PingFangTC-Regular", size: 12)
+            titleLabel.adjustsFontSizeToFitWidth = true
+            contentLabel.adjustsFontSizeToFitWidth = true
+            
+            btn.append(UIButton())
+            btn[i].translatesAutoresizingMaskIntoConstraints = false
+            
+            
+            btn[i].addSubview(icon)
+            btn[i].addSubview(titleLabel)
+            btn[i].addSubview(contentLabel)
+            
+            btn[i].setImage(UIImage(named: bgArr[i]), for: .normal)
+            icon.image = UIImage(named: iconArr[i])
+            titleLabel.text = titleLabelArr[i]
+            contentLabel.text = contentLabelArr[i]
+            
+            icon.topAnchor.constraint(equalTo: btn[i].topAnchor, constant: 16).isActive = true
+            icon.leadingAnchor.constraint(equalTo: btn[i].leadingAnchor, constant: 16).isActive = true
+            icon.widthAnchor.constraint(equalToConstant: 32).isActive = true
+            icon.heightAnchor.constraint(equalToConstant: 32).isActive = true
+            
+            titleLabel.topAnchor.constraint(equalTo: btn[i].topAnchor, constant: 11).isActive = true
+            titleLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 12).isActive = true
+
+            
+            contentLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+            contentLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 12).isActive = true
+
+        }
+        return btn
+    }()
+    let stack:UIStackView = {
         let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .horizontal
         stack.distribution = .fillEqually
-        stack.alignment = .fill
-        stack.translatesAutoresizingMaskIntoConstraints = false
+        
         return stack
     }()
+//    let fnButtonUp: [UIButton] = {
+//        var btn:[UIButton] = []
+//        let icon = UIImageView()
+//        icon.translatesAutoresizingMaskIntoConstraints = false
+//        let titleLabel = UILabel()
+//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+//        let contentLabel = UILabel()
+//        contentLabel.translatesAutoresizingMaskIntoConstraints = false
+//
+//        let titleLabelArr:[String] = ["速配車款","立即預選"]
+//        let contentLabelArr:[String] = ["輸入條件篩選", "預選下一台愛車"]
+//        let iconArr:[String] = ["icon_img_buycar", "icon_img_filterCars"]
+//        let bgArr:[String] = ["icon_img_background_left", "icon_img_background_right"]
+//
+//        for i in 0..<2{
+//            btn.append(UIButton())
+//            btn[i].widthAnchor.constraint(equalToConstant: 172).isActive = true
+//            btn[i].heightAnchor.constraint(equalToConstant: 64).isActive = true
+//
+//            btn[i].addSubview(icon)
+//            btn[i].addSubview(titleLabel)
+//            btn[i].addSubview(contentLabel)
+//
+//            icon.frame = CGRect(x: 16, y: 16, width: 32, height: 32)
+//            titleLabel.frame = CGRect(x: 60, y: 11, width: 56, height: 31)
+//            contentLabel.frame = CGRect(x: 60, y: 33, width: 72, height: 20)
+//
+//            btn[i].translatesAutoresizingMaskIntoConstraints = false
+//            btn[i].setImage(UIImage(named: bgArr[i]), for: .normal)
+//            icon.image = UIImage(named: iconArr[i])
+//            titleLabel.text = titleLabelArr[i]
+//            contentLabel.text = contentLabelArr[i]
+//        }
+//        return btn
+//    }()
+//    let fnButtonDown:[UIButton] = {
+//        var btn:[UIButton] = []
+//        for i in 0..<4{
+//            btn.append(UIButton())
+//        }
+//        return btn
+//    }()
+//    let stack:UIStackView = {
+//       let stack = UIStackView()
+//        stack.translatesAutoresizingMaskIntoConstraints = false
+//        stack.axis = .horizontal
+//        stack.distribution = .fillEqually
+//        stack.alignment = .center
+//        return stack
+//    }()
+//    let stack: [UIStackView] = {
+//        var stack:[UIStackView] = []
+//        for i in 0..<2{
+//            stack.append(UIStackView())
+//            stack[i].axis = .horizontal
+//            stack[i].distribution = .fillEqually
+//            stack[i].alignment = .fill
+//            stack[i].translatesAutoresizingMaskIntoConstraints = false
+//        }
+//        return stack
+//    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -54,6 +201,12 @@ class ViewController: UIViewController {
         view.addSubview(bottomView)
         naviBoundView.addSubview(logoImageView)
         naviBoundView.addSubview(notifyButton)
+        
+        stack.addArrangedSubview(testButton1[0])
+        stack.addArrangedSubview(testButton1[1])
+//        stack.addArrangedSubview(testButton1)
+        view.addSubview(stack)
+        
         setupHomepageLayout()
     }
     func setupHomepageLayout(){
@@ -64,27 +217,30 @@ class ViewController: UIViewController {
         naviBoundView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         naviBoundView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         naviBoundView.heightAnchor.constraint(equalToConstant: 128.0).isActive = true
-        naviBoundView.bottomAnchor.constraint(equalTo:bottomView.topAnchor).isActive = true
+//        naviBoundView.bottomAnchor.constraint(equalTo:bottomView.topAnchor).isActive = true
 
         logoImageView.widthAnchor.constraint(equalToConstant: 160).isActive = true
         logoImageView.heightAnchor.constraint(equalToConstant: 28).isActive = true
         logoImageView.topAnchor.constraint(equalTo: naviBoundView.topAnchor, constant: 60).isActive = true
-//        logoImageView.bottomAnchor.constraint(equalTo: naviBoundView.bottomAnchor, constant: 40).isActive = true
         logoImageView.leadingAnchor.constraint(equalTo: naviBoundView.leadingAnchor, constant: 16).isActive = true
-//        logoImageView.trailingAnchor.constraint(equalTo: naviBoundView.trailingAnchor, constant: 199).isActive = true
         
         notifyButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
         notifyButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
         notifyButton.topAnchor.constraint(equalTo: naviBoundView.topAnchor, constant: 63).isActive = true
         notifyButton.trailingAnchor.constraint(equalTo: naviBoundView.trailingAnchor, constant: -16).isActive = true
-//        actionButton.leadingAnchor.constraint(equalTo: logoImageView.trailingAnchor, constant: 159).isActive = true
         
         // MARK: setup bottom view
-        bottomView.topAnchor.constraint(equalTo: naviBoundView.bottomAnchor).isActive = true
+        bottomView.topAnchor.constraint(equalTo: naviBoundView.bottomAnchor, constant: -24.0).isActive = true
         bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
+        
+        stack.topAnchor.constraint(equalTo: bottomView.topAnchor).isActive = true
+        stack.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 16).isActive = true
+        stack.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -16).isActive = true
+        stack.heightAnchor.constraint(equalToConstant: 64).isActive = true
     }
+
 }
 
